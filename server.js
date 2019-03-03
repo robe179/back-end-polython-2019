56d391fc6ed1b78cb2087b79c313a62c57ad7881
Luis A. Espinosa B <Luis@Luiss-MacBook-Pro-Tlamach-2.local>
2019-03-03 06:01:01 -0600

    Se parametrizó  API de cara a volverla reutilizable

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express'),
   port = 3002;
 
 var path = require('path');
-const dialogflow = require('./prueba');
 
 app.listen(port);
 
@@ -15,36 +14,34 @@ app.use( function(req, res, next){
 });
 var requestjson = require( "request-json" );
 
-let ticker = "https://api.bitso.com/v3/ticker/?book=btc_mxn";
-let requestTicker = requestjson.createClient( ticker );
-
 app.get('/v1/ticker', (req, res, next) => {
-  console.log("\n" + new Date() + " -- obteniendo tickers ");
+
+  console.log("\n" + new Date() + " -- Obteniendo tickers ");
+
+  let ticker = "https://api.bitso.com/v3/ticker/?book=" + req.query.book;
+  console.log( "  ticker = " + ticker );
+
+  if( req.query.book == 'undefined' )
+    res.status(400).send("Book no encontrado.");
+
+  let requestTicker = requestjson.createClient( ticker );
+
   requestTicker.get('', (err, resM, body) => {
     if(err){
       console.error(err);
       res.status(500).send("Hubo algun problema con la conexión");
     }else {
       var hour = body.payload.created_at.substring(11, 16);
-      var arrayDataResonse = [hour, parseInt(body.payload.low), parseInt(body.payload.bid), parseInt(body.payload.ask),parseInt( body.payload.high)];
+      var arrayDataResonse = [ hour
+                             , parseFloat( body.payload.low  )
+                             , parseFloat( body.payload.bid  )
+                             , parseFloat( body.payload.ask  )
+                             , parseFloat( body.payload.high ) ];
       console.log(arrayDataResonse);
       res.send(arrayDataResonse);
     }
   });
 });
-
- // -********************* dialogflow  *******************-
-
- app.get('/v1/dialog', (req, res, next) => {
-   console.log("Vemos valor post ", req.query.question);
-   const promise = dialogflow.runSample("bitbot-polython", req.query.question);
-   promise.then( data => {
-     console.log(data);
-     res.send(data);
-   }).catch(error => {
-     console.log("error ", error)
-   });
- });
 
 app.listen(3000, function () {
   console.log('polython 2019 app listening on port ' + port);
