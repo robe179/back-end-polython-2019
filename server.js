4a4fc86a10fd01094df53ff9678d515172bf9214
Roberto Brito <rbritoa@axtel.com.mx>
2019-03-03 00:40:40 -0600

    Creamos consulta de ticker bitso

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 var path = require('path');
 
-app.listen(port);
+app.listen(port, "100.112.24.161");
 
 app.use( function(req, res, next){
   res.header( "Access-Control-Allow-Origin", "*" );
@@ -12,7 +12,25 @@ app.use( function(req, res, next){
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
+var requestjson = require( "request-json" );
 
+let ticker = "https://api.bitso.com/v3/ticker/?book=btc_mxn";
+let requestTicker = requestjson.createClient( ticker );
+
+app.get('/v1/ticker', (req, res, next) => {
+  console.log("\n" + new Date() + " -- obteniendo tickers ");
+  requestTicker.get('', (err, resM, body) => {
+    if(err){
+      console.error(err);
+      res.status(500).send("Hubo algun problema con la conexión");
+    }else {
+      var hour = body.payload.created_at.substring(11, 16);
+      var arrayDataResonse = [hour, parseInt(body.payload.low), parseInt(body.payload.bid), parseInt(body.payload.ask),parseInt( body.payload.high)];
+      console.log(arrayDataResonse);
+      res.send(arrayDataResonse);
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log('polython 2019 app listening on port ' + port);
